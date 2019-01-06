wspr/lualdap

commit 0b455a530a9ac9f25746bdea54016174a0eaf12c

Fix segfault in lualdap_open_simple()

src/lualdap.c
@@ -1013,7 +1013,7 @@ static int lualdap_open_simple (lua_State *L) {
 	int use_tls = lua_toboolean (L, 4);
 	conn_data *conn = (conn_data *)lua_newuserdata (L, sizeof(conn_data));
 #if defined(LDAP_API_FEATURE_X_OPENLDAP) && LDAP_API_FEATURE_X_OPENLDAP >= 20300
-	struct berval cred = { 0, NULL };
+	struct berval *cred = NULL;
 	char *host_with_schema = NULL;
 #endif
 	int err;
@@ -1047,12 +1047,9 @@ static int lualdap_open_simple (lua_State *L) {
 	}
 	/* Bind to a server */
 #if defined(LDAP_API_FEATURE_X_OPENLDAP) && LDAP_API_FEATURE_X_OPENLDAP >= 20300
-	cred.bv_len = strlen(password);
-	cred.bv_val = malloc(cred.bv_len+1);
-	strcpy(cred.bv_val, password);
-	err = ldap_sasl_bind_s (conn->ld, who, LDAP_SASL_SIMPLE, &cred, NULL, NULL, NULL);
-	free(cred.bv_val);
-	memset(&cred, 0, sizeof(cred));
+	cred = ber_bvstrdup(password);
+	err = ldap_sasl_bind_s (conn->ld, who, LDAP_SASL_SIMPLE, cred, NULL, NULL, NULL);
+	ber_bvfree(cred);
 #else
 	err = ldap_bind_s (conn->ld, who, password, LDAP_AUTH_SIMPLE);
 #endif

commit 7a3864cfe301ebe5acbd2ec7bcd199f8d7fab066

Fix segfault in lualdap_bind_simple()
The same code was used in `lualdap_open_simple`, but not fixed in
0b455a530a9ac9f25746bdea54016174a0eaf12c.

Fixes: 0b455a530a9ac9f25746bdea54016174a0eaf12c

src/lualdap.c
@@ -501,19 +501,15 @@ static int lualdap_bind_simple (lua_State *L) {
 	const char *password = luaL_checkstring (L, 3);
 	int err;
 #if defined(LDAP_API_FEATURE_X_OPENLDAP) && LDAP_API_FEATURE_X_OPENLDAP >= 20300
-	struct berval cred = { 0, NULL };
-	cred.bv_len = strlen(password);
-	cred.bv_val = malloc(cred.bv_len+1);
-	strcpy(cred.bv_val, password);
-	err = ldap_sasl_bind_s (conn->ld, who, LDAP_SASL_SIMPLE, &cred, NULL, NULL, NULL);
-	free(cred.bv_val);
-	memset(&cred, 0, sizeof(cred));
+	struct berval *cred = ber_bvstrdup(password);
+	err = ldap_sasl_bind_s (conn->ld, who, LDAP_SASL_SIMPLE, cred, NULL, NULL, NULL);
+	ber_bvfree(cred);
 #else
 	err = ldap_simple_bind_s (conn->ld, who, password);
 #endif
 	if (err != LDAP_SUCCESS)
 		return faildirect (L, ldap_err2string (err));
-	
+
 	lua_pushboolean (L, 1);
 	return 1;
 }